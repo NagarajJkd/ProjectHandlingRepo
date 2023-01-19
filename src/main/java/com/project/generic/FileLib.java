bae46516b648c4b33f1b8539589156874d5f0dd5
JORAPUR <JORAPUR@DESKTOP-LR7I5H0>
2023-01-19 15:36:13 +0530

    2nd commit

--- src/main/java/com/project/generic/FileLib.java
+++ src/main/java/com/project/generic/FileLib.java
@@ -1,5 +1,12 @@
 package com.project.generic;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class FileLib {
 
+	@Test
+	public void readDataFromPropertyfile() {
+		Reporter.log("Read data from property file", true);
+	}
 }
