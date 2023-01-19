c23e26bc7856a3b269f729a42aba7ca551cff017
JORAPUR <JORAPUR@DESKTOP-LR7I5H0>
2023-01-19 15:51:46 +0530

    1

---
+++ src/main/java/com/project/generic/DataBaseUtility.java
@@ -0,0 +1,22 @@
+package com.project.generic;
+
+import org.testng.Reporter;
+
+public class DataBaseUtility {
+
+	
+	public void connectToDB() {
+		
+		
+		
+		
+		
+		
+		Reporter.log("Connecting to the data base", true);
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/main/java/com/project/generic/ExcellUtility.java
@@ -0,0 +1,10 @@
+package com.project.generic;
+
+import org.testng.Reporter;
+
+public class ExcellUtility {
+
+	public void readDataFromExcell() {
+		Reporter.log("reading the data from excell", true);
+	}
+}
